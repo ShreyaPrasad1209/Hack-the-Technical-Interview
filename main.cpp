281987ca7fa15f1c6b142e063203f83d3dec7a88
PavanMadushanka <PavanMadushanka@users.noreply.github.com>
2020-10-21 10:18:41 -0400

    Solution for problem 1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  int n;
+  std::cout << "Enter n: ";
+  std::cin >> n;
+  for (int i = 1; i <= n; i++) {
+    if(i%3==0 && i%5==0) {
+      std::cout << "FizzBuzz\n";
+    }
+    else if (i%3==0) {
+      std::cout << "Fizz\n";
+    }
+    else if (i%5==0) {
+      std::cout << "Buzz\n";
+    }
+    else {
+      std::cout << i << "\n";
+    }
+    
+  }
+}
